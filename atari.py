f26f48668ca1e088c97741b34e1aa6dc5a3b0139
Raphael Stäbler <rs@instanz.ch>
2016-07-09 22:56:56 +0200

    Encode frame history in color channel

--- atari.py
+++ atari.py
@@ -20,7 +20,7 @@ class ExperienceReplay(object):
 	def get_batch(self, model, batch_size):
 		len_memory = len(self.memory)
 		num_actions = 6
-		inputs = np.zeros((min(len_memory, batch_size), 210, 160, 1))
+		inputs = np.zeros((min(len_memory, batch_size), 4, 210, 160))
 		targets = np.zeros((inputs.shape[0], num_actions))
 		for i, idx in enumerate(np.random.randint(0, len_memory, size=inputs.shape[0])):
 			input_t, action_t, reward_t, input_tp1 = self.memory[idx][0]
@@ -28,8 +28,8 @@ class ExperienceReplay(object):
 
 			inputs[i:i+1] = input_t
 
-			targets[i] = model.predict(input_t.reshape(1, 210, 160, 1))[0]
-			q_next = np.max(model.predict(input_tp1.reshape(1, 210, 160, 1))[0])
+			targets[i] = model.predict(input_t.reshape(1, 4, 210, 160))[0]
+			q_next = np.max(model.predict(input_tp1.reshape(1, 4, 210, 160))[0])
 
 			if game_over:
 				targets[i, action_t] = reward_t
@@ -39,7 +39,7 @@ class ExperienceReplay(object):
 		return inputs, targets
 
 def make_grey(x):
-	return np.average(x, 2).reshape(210, 160, 1)
+	return np.average(x, 2)
 
 if __name__ == "__main__":
 	episodes = 1000
@@ -51,18 +51,18 @@ if __name__ == "__main__":
 	model = Sequential()
 	model.add(Convolution2D(32, 8, 8,
 		subsample=(4, 4),
-		dim_ordering='tf',
+		dim_ordering='th',
 		border_mode='same',
-		input_shape=(210, 160, 1),
+		input_shape=(4, 210, 160),
 		activation='relu'))
 	model.add(Convolution2D(64, 4, 4,
 		subsample=(2, 2),
-		dim_ordering='tf',
+		dim_ordering='th',
 		border_mode='same',
 		activation='relu'))
 	model.add(Convolution2D(64, 3, 3,
 		subsample=(1, 1),
-		dim_ordering='tf',
+		dim_ordering='th',
 		border_mode='same',
 		activation='relu'))
 	model.add(Flatten())
@@ -79,17 +79,20 @@ if __name__ == "__main__":
 	for i_episode in range(episodes):
 		loss = 0.
 		frame = 0
-		observation = env.reset() # shape (210, 160, 3)
-		input = make_grey(observation)
 		game_over = False
+		input = np.zeros((4, 210, 160))
+		observation = env.reset() # shape (210, 160, 3)
+		input[frame] = make_grey(observation)
 		action = env.action_space.sample()
 
 		while not game_over:
 			env.render()
 
-			if frame%skip_frames != 0:
+			frame_iteration = frame%skip_frames
+
+			if frame_iteration != 3:
 				observation, reward, game_over, info = env.step(action)
-				input = make_grey(observation)
+				input[frame_iteration] = make_grey(observation)
 				win_count += reward
 			else:
 				input_tm1 = input
@@ -97,11 +100,11 @@ if __name__ == "__main__":
 				if np.random.rand() <= epsilon:
 					action = env.action_space.sample()
 				else:
-					q = model.predict(input.reshape(1, 210, 160, 1))
+					q = model.predict(input.reshape(1, 4, 210, 160))
 					action = np.argmax(q)
 
 				observation, reward, game_over, info = env.step(action)
-				input = make_grey(observation)
+				input[frame_iteration] = make_grey(observation)
 				win_count += reward
 
 				exp_replay.remember([input_tm1, action, reward, input], game_over)
