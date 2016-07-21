d10ec664080f2a7ee76be6a300b19ebecf4703dd
Raphael Stäbler <rs@instanz.ch>
2016-07-21 21:55:19 +0200

    New exploration algorithm and multiple episodes per batch

--- pg-atari.py
+++ pg-atari.py
@@ -19,10 +19,11 @@ def preprocess(x):
 	return x_new
 
 if __name__ == "__main__":
-	epsilon = 1. # exploration
-	epsilon_degrade = 1e-4
+	epsilon = .2 # exploration
+	epsilon_degrade = 0
 	epsilon_min = .1
 	discount = .99
+	episodes_per_batch = 10
 
 	model = Sequential()
 
@@ -64,6 +65,7 @@ if __name__ == "__main__":
 	rewards = []
 	losses = []
 	scores = []
+	mean_scores = []
 	meanqs = []
 	e = epsilon
 
@@ -79,7 +81,7 @@ if __name__ == "__main__":
 
 		# determine next action
 		q = model.predict(x.reshape(1, 2, 80, 74))[0]
-		action = np.random.choice(6, 1, p=q/np.sum(q))[0]
+		action = np.argmax(q) if np.random.rand() > e else np.random.choice(6, 1, p=q/np.sum(q))[0]
 
 		y = q.copy()
 		y[action] = 1. # Encourage current action
@@ -96,63 +98,64 @@ if __name__ == "__main__":
 
 		if terminal:
 			episode += 1
-
-			exs = np.asarray(xs)
-			eqs = np.asarray(qs)
-			eys = np.asarray(ys)
-			erewards = np.asarray(rewards)
-
-			# discount rewards
-			dr = np.zeros(erewards.shape)
-			ra = 0
-			for i in reversed(range(0, erewards.size)):
-				if erewards[i] != 0: ra = 0 # Pong specific?
-				ra = ra * discount + erewards[i]
-				dr[i] = ra
-			dr -= np.mean(dr)
-			dr /= np.std(dr) if np.std(dr) > 0. else 1.
-
-			for i,a in enumerate(actions):
-				eys[i][a] *= dr[i]
-
-
-			loss = model.train_on_batch(exs, eys)
-
-
-			meanq = np.mean(np.max(qs, axis=1))
-			losses.append(loss)
-			scores.append(score)
-			meanqs.append(meanq)
-
-			# log
-			print "Episode %d, loss %f, mean q %f, epsilon %f, score %d"%(episode, loss, meanq, e, score)
-
-			# plot
-			if episode > 1:
-				plt.close()
-				s_loss = plt.subplot(311)
-				s_q = plt.subplot(312)
-				s_score = plt.subplot(313)
-				s_loss.plot(range(0, episode), losses, 'b-')
-				s_q.plot(range(0, episode), meanqs, 'b-')
-				s_score.plot(range(0, episode), scores, 'b-')
-				plt.show(block=False)
-
-			# reset
-			xs = []
-			qs = []
-			ys = []
-			actions = []
-			rewards = []
 			prev_x = np.zeros((80, 74))
+			scores.append(score)
 			score = 0
 			observation = env.reset()
 
-			if e > epsilon_min:
-				e = epsilon - epsilon_degrade*episode
+			if episode%episodes_per_batch == 0:
+				exs = np.asarray(xs)
+				eqs = np.asarray(qs)
+				eys = np.asarray(ys)
+				erewards = np.asarray(rewards)
 
-			# save model and plot
-			if episode%10 == 0:
+				# discount rewards
+				dr = np.zeros(erewards.shape)
+				ra = 0
+				for i in reversed(range(0, erewards.size)):
+					if erewards[i] != 0: ra = 0 # Pong specific?
+					ra = ra * discount + erewards[i]
+					dr[i] = ra
+					#print "discount %d:%f -> %f"%(i, erewards[i], dr[i])
+				dr -= np.mean(dr)
+				dr /= np.std(dr) if np.std(dr) > 0. else 1.
+
+				for i,a in enumerate(actions):
+					eys[i][a] *= dr[i]
+
+				loss = model.train_on_batch(exs, eys)
+
+				meanq = np.mean(np.max(qs, axis=1))
+				mean_score = np.mean(scores)
+				losses.append(loss)
+				mean_scores.append(mean_score)
+				meanqs.append(meanq)
+
+				# log
+				print "Episode %d, loss %f, mean q %f, epsilon %f, mean score %f"%(episode, loss, meanq, e, mean_score)
+
+				# plot
+				if episode > 10:
+					plt.close()
+					s_loss = plt.subplot(311)
+					s_q = plt.subplot(312)
+					s_score = plt.subplot(313)
+					s_loss.plot(range(0, episode), losses, 'b-')
+					s_q.plot(range(0, episode), meanqs, 'b-')
+					s_score.plot(range(0, episode), mean_scores, 'b-')
+					plt.show(block=False)
+
+				# reset
+				xs = []
+				qs = []
+				ys = []
+				actions = []
+				rewards = []
+
+				if e > epsilon_min:
+					e = epsilon - epsilon_degrade*episode
+
+				# save model and plot
 				print "Saving weights to disk..."
 				model.save_weights('pg-atari_weights.h5', overwrite=True)
 				print "Saving plot to disk..."
