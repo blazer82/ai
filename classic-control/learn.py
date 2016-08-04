c262a0a8fe1f4e1d29940dac85d9b2b6b07fd557
Raphael Stäbler <rs@instanz.ch>
2016-08-04 17:49:40 +0200

    Added classic control environment

---
+++ classic-control/learn.py
@@ -0,0 +1,10 @@
+from agent import Agent
+from environment import Environment
+from model import Model
+
+model = Model(batch_size=32, load=None)
+env = Environment(env_type=Environment.TYPE_CART_POLE, render=True)
+agent = Agent(env=env, model=model)
+
+while True:
+	agent.learn(overfit=False)
---
+++ classic-control/model.py
@@ -0,0 +1,35 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Flatten, Activation
+from keras.layers.convolutional import Convolution1D
+from keras.optimizers import SGD
+
+
+class Model:
+	def __init__(self, activation='tanh', batch_size=32, load=None):
+		self.batch_size = batch_size
+		self.model = Sequential()
+
+		self.model.add(Convolution1D(4, 1, input_shape=(2, 4)))
+		self.model.add(Activation(activation))
+
+		self.model.add(Flatten())
+
+		self.model.add(Dense(4))
+		self.model.add(Activation(activation))
+
+		self.model.add(Dense(2))
+		self.model.add(Activation('softmax'))
+
+		if load != None:
+			self.model.load_weights(load)
+
+		self.model.compile(SGD(), loss='mse')
+
+	def predict(self, X):
+		return self.model.predict(X.reshape((1,) + X.shape))[0]
+
+	def learn(self, X, y):
+		return self.model.fit(X, y, nb_epoch=1, batch_size=self.batch_size, shuffle=True, verbose=1)
+
+	def save(self, filename):
+		return self.model.save_weights(filename, overwrite=True)
